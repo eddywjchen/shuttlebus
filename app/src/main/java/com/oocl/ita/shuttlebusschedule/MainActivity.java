0fca3d6272761e09de441c42c374ca7b66d75f13
chened2 <eddy.chen@oocl.com>
2016-01-07 22:12:13 +0800

    update

--- app/src/main/java/com/oocl/ita/shuttlebusschedule/MainActivity.java
+++ app/src/main/java/com/oocl/ita/shuttlebusschedule/MainActivity.java
@@ -4,8 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.oocl.ita.shuttlebusschedule.entity.User;
+import com.oocl.ita.shuttlebusschedule.service.UserService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_statistics);
-        setStationStatisticsData();
+        setStationStatisticsMockupData();
         }
 
     @Override
@@ -39,7 +49,35 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setStationStatisticsData() {
-        User user = new User();
+    private final class ItemClickListener implements AdapterView.OnItemClickListener {
+
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            ListView listView = (ListView) parent;
+            HashMap<String, Object> data = (HashMap<String, Object>) listView.getItemAtPosition(position);
+            String personId = data.get("id").toString();
+            Toast.makeText(getApplicationContext(), personId, Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private void setStationStatisticsMockupData() {
+        ListView listView = (ListView) this.findViewById(R.id.stationStatisticsView);
+        UserService userService = new UserService();
+        List<User> users = userService.getScrollData();
+        List<HashMap<String, Object>> data = new ArrayList<HashMap<String,Object>>();
+        for(User user : users){
+            HashMap<String, Object> item = new HashMap<String, Object>();
+//            item.put("id", user.getId());
+            item.put("username", user.getUsername());
+//            item.put("dept", user.getDept());
+            item.put("boardingStation", user.getBoardingStation());
+            item.put("egressStation", user.getEgressStation());
+            data.add(item);
+        }
+        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.user_item,
+                new String[]{"username", "boardingStation", "egressStation"}, new int[]{R.id.username, R.id.boardingStation, R.id.egressStation});
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new ItemClickListener());
+
     }
 }
