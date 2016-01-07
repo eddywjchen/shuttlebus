408b960b6498a7c0a9bb6df39cc18e590623065d
liuja8 <jacob.liu@cargosmart.com>
2016-01-08 01:11:02 +0800

    add the tab function in the main page

--- app/src/main/java/com/oocl/ita/shuttlebusschedule/MainActivity.java
+++ app/src/main/java/com/oocl/ita/shuttlebusschedule/MainActivity.java
@@ -1,14 +1,18 @@
 package com.oocl.ita.shuttlebusschedule;
 
+import android.app.ActivityGroup;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.oocl.ita.shuttlebusschedule.entity.User;
 import com.oocl.ita.shuttlebusschedule.service.StationService;
@@ -19,16 +23,37 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActivityGroup {
     private StationService service;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_station_statistics);
-        setStationStatisticsMockupData();
+        setContentView(R.layout.activity_main);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        // 如果不是继承TabActivity，则必须在得到tabHost之后，添加标签之前调用tabHost.setup()
 
-        setRouteStationData();
+        tabHost.setup(this.getLocalActivityManager());
+        // 这里content的设置采用了布局文件中的view
+        Intent intent = new Intent(this, UserRidingActivity.class);
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("Home").setContent(R.id.tab1));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Summary")
+                .setContent(intent));
+        updateTab(tabHost);
+        //setStationStatisticsMockupData();
+        //setRouteStationData();
+    }
+
+    private void updateTab(TabHost tabHost) {
+        for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
+            View view = tabHost.getTabWidget().getChildAt(i);
+            TextView tv = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tv.setTextSize(16);
+            tv.setTypeface(Typeface.DEFAULT);
+            tv.setTextColor(Color.WHITE);// 设置字体和风格
         }
+    }
 
     private void setRouteStationData() {
         ListView listView = (ListView) this.findViewById(R.id.routeStationView);
@@ -36,8 +61,8 @@ public class MainActivity extends AppCompatActivity {
         service = new StationService(this);
         List<Map<String, String>> stations = service.getData();
         SimpleAdapter routeStationAdapter = new SimpleAdapter(this, stations, R.layout.route_station_summary,
-                new String[]{"routeName", "stationName", "regularUserCount","realTimeUserCount","changedCount"},
-                new int[]{R.id.routeName, R.id.stationName, R.id.regularUserCount,R.id.realTimeUserCount,R.id.changedCount});
+                new String[]{"routeName", "stationName", "regularUserCount", "realTimeUserCount", "changedCount"},
+                new int[]{R.id.routeName, R.id.stationName, R.id.regularUserCount, R.id.realTimeUserCount, R.id.changedCount});
         listView.setAdapter(routeStationAdapter);
         listView.setOnItemClickListener(new ItemClickListener());
     }
@@ -79,8 +104,8 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) this.findViewById(R.id.stationStatisticsView);
         UserService userService = new UserService();
         List<User> users = userService.getScrollData();
-        List<HashMap<String, Object>> data = new ArrayList<HashMap<String,Object>>();
-        for(User user : users){
+        List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+        for (User user : users) {
             HashMap<String, Object> item = new HashMap<String, Object>();
 //            item.put("id", user.getId());
             item.put("username", user.getUsername());
