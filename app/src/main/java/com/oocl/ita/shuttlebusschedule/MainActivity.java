5975ee71cf2deb952d08f99eb651018c6f05f4b5
chened2 <eddy.chen@oocl.com>
2016-01-07 21:10:27 +0800

    Revert "add the user entity"
    This reverts commit 01c10e0fa7c94a223f4d00a1524ddfc1e51d9a99.

--- app/src/main/java/com/oocl/ita/shuttlebusschedule/MainActivity.java
+++ app/src/main/java/com/oocl/ita/shuttlebusschedule/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.station_statistics);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
